5d03edae04e5997aea27f15dd9806caef21dde7b
brkncoyot3 <brkncoyot3@gmail.com>
2023-03-26 19:59:13 -0400

    refactor(loadScripts.js): rename loadScript function to lsc refactor(loadScripts.js): split script loading into separate functions for each script feat(loadScripts.js): add error handling for script loading promises

--- general/loadScripts.js
+++ general/loadScripts.js
@@ -1,7 +1,7 @@
 /*
  * Loads a JavaScript file and returns a Promise for when it is loaded
  */
-const loadScript = (src) => {
+const lsc = (src) => {
     return new Promise((resolve, reject) => {
         const script = document.createElement("script");
         script.type = "text/javascript";
@@ -11,23 +11,27 @@ const loadScript = (src) => {
         document.body.append(script);
     });
 };
-loadScript("https://supa.illusory.io/storage/v1/object/public/js/clerk/loadClerk.js")
-    .then(() =>
-        console.log("Clerk loaded"),
-        loadScript("https://supa.illusory.io/storage/v1/object/public/js/proxies/getProxies.js")
-            .then(() =>
-                console.log("Get Proxies loaded"),
-                loadScript(
-                    "https://supa.illusory.io/storage/v1/object/public/js/supa/supaToken.js"
-                )
-            )
-            .then(() =>
-                console.log("Supa Token loaded"),
-                loadScript(
-                    "https://supa.illusory.io/storage/v1/object/public/js/supa/supaClerk.js"
-                )
-            )
-            .then(() =>
-                console.log("Supa Clerk loaded")
-            )
-            .catch(() => console.error("Something went wrong.")))
+
+function sc_loadClerk() {
+    lsc("https://supa.illusory.io/storage/v1/object/public/js/clerk/loadClerk.js")
+        .then(() => console.log("Clerk loaded"))
+        .catch((err) => console.error(err.message));
+}
+
+function sc_getProxies() {
+    lsc("https://supa.illusory.io/storage/v1/object/public/js/proxies/getProxies.js")
+        .then(() => console.log("Get Proxies loaded"))
+        .catch((err) => console.error(err.message));
+}
+
+function sc_supaToken() {
+    lsc("https://supa.illusory.io/storage/v1/object/public/js/supa/supaToken.js")
+        .then(() => console.log("Supa Token loaded"))
+        .catch((err) => console.error(err.message));
+}
+
+function sc_upaClerk() {
+    lsc("https://supa.illusory.io/storage/v1/object/public/js/supa/supaClerk.js")
+        .then(() => console.log("Supa Clerk loaded"))
+        .catch((err) => console.error(err.message));
+}
