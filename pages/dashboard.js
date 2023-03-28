fb163fd1fe61beff0115093a807b3d7adb1c58b1
brkncoyot3 <brkncoyot3@gmail.com>
2023-03-28 12:39:47 -0400

    refactor(dashboard.js): fix indentation and formatting of loadClerk() function refactor(dashboard.js): fix indentation and formatting of lsc() function
    feat(dashboard.js): add async/await to lscSupaToken, lscSupaClerk, and lscGetProxies functions
    feat(dashboard.js): add lscTimeAgo function to load timeago.js library
    feat(dashboard.js): add play function to load all necessary scripts on page load

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,23 +3,23 @@
 // Load Clerk
 async function loadClerk() {
     return new Promise((resolve) => {
-      // Get this URL from the Clerk Dashboard
-      const frontendApi = "clerk.a7i81.ec7ck.lcl.dev";
-      const version = "@latest"; // Set to appropriate version
-  
-      // Creates asyncronous script
-      const script = document.createElement("script");
-      script.setAttribute("data-clerk-frontend-api", frontendApi);
-      script.async = true;
-      script.src = `https://${frontendApi}/npm/@clerk/clerk-js${version}/dist/clerk.browser.js`;
-      document.body.appendChild(script);
-      resolve();
-    });
-  }
+        // Get this URL from the Clerk Dashboard
+        const frontendApi = "clerk.a7i81.ec7ck.lcl.dev";
+        const version = "@latest"; // Set to appropriate version
 
-  /*
- * Loads a JavaScript file and returns a Promise for when it is loaded
- */
+        // Creates asyncronous script
+        const script = document.createElement("script");
+        script.setAttribute("data-clerk-frontend-api", frontendApi);
+        script.async = true;
+        script.src = `https://${frontendApi}/npm/@clerk/clerk-js${version}/dist/clerk.browser.js`;
+        document.body.appendChild(script);
+        resolve();
+    });
+}
+
+/*
+* Loads a JavaScript file and returns a Promise for when it is loaded
+*/
 const lsc = (src) => {
     return new Promise((resolve, reject) => {
         const script = document.createElement("script");
@@ -32,20 +32,36 @@ const lsc = (src) => {
 };
 
 
-function lscSupaToken() {
+async function lscSupaToken() {
     lsc("https://supa.illusory.io/storage/v1/object/public/js/supa/supaToken.js")
         .then(() => console.log("Supa Token js loaded"))
         .catch((err) => console.error(err.message));
 }
 
-function lscSupaClerk() {
+async function lscSupaClerk() {
     lsc("https://supa.illusory.io/storage/v1/object/public/js/supa/supaClerk.js")
         .then(() => console.log("Supa Clerk js loaded"))
         .catch((err) => console.error(err.message));
 }
 
-function lscGetProxies() {
+async function lscGetProxies() {
     lsc("https://supa.illusory.io/storage/v1/object/public/js/proxies/getProxies.js")
         .then(() => console.log("Get Proxies js loaded"))
         .catch((err) => console.error(err.message));
 }
+
+async function lscTimeAgo() {
+    lsc("https://cdnjs.cloudflare.com/ajax/libs/timeago.js/4.0.2/timeago.full.min.js")
+        .then(() => console.log("Get Proxies js loaded"))
+        .catch((err) => console.error(err.message));
+}
+
+async function play() {
+    await lscSupaToken();
+    await lscSupaClerk();
+    await lscGetProxies();
+    await lscTimeAgo();
+    await loadClerk();
+}
+
+play();
