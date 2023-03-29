0e4d04e64ddfdc32fd8f50c62c988cacff1159c6
brkncoyot3 <brkncoyot3@gmail.com>
2023-03-29 11:33:43 -0400

    style(dashboard.js): add whitespace before comment in loadClerk function

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,6 +7,7 @@ async function loadClerk() {
             // Get this URL from the Clerk Dashboard
             const frontendApi = "clerk.a7i81.ec7ck.lcl.dev";
             const version = "@latest"; // Set to appropriate version
+
             // Creates asyncronous script
             const script = document.createElement("script");
             script.setAttribute("data-clerk-frontend-api", frontendApi);
