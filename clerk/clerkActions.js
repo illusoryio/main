e21b0f503d9ac5fdca221408137ddd954a81fd4f
brkncoyot3 <brkncoyot3@gmail.com>
2023-03-30 12:59:27 -0400

    refactor(clerkActions.js): remove unnecessary whitespace and add missing newline at end of file

--- clerk/clerkActions.js
+++ clerk/clerkActions.js
@@ -1,13 +1,13 @@
 async function clerkActions(action) {
-    // Authenticate request
-    const token = await supaToken();
-    console.log("supaToken", token);
-    const supc = await supaClerk(token);
-    console.log("supaClerk", supc);
-  
-    // Get Proxies
-    if (action == "user_get_proxies") {
-      const supcli = await getProxies(supc);
-      console.log("Get Proxies", supcli);
-    }
+  // Authenticate request
+  const token = await supaToken();
+  console.log("supaToken", token);
+  const supc = await supaClerk(token);
+  console.log("supaClerk", supc);
+
+  // Get Proxies
+  if (action == "user_get_proxies") {
+    const supcli = await getProxies(supc);
+    console.log("Get Proxies", supcli);
   }
+}
