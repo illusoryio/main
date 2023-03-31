bfda5e5482c399877300ebb7b16d55709f27ec72
brkncoyot3 <brkncoyot3@gmail.com>
2023-03-31 10:30:27 -0400

    chore(dashboard.js): comment out unused code and remove trailing newline

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -130,13 +130,13 @@ async function clerkResolved() {
     // await getTimeAgoAuto();
 
     await lscClerkActions();
-    const action = "user_get_proxies";
-    await clerkActions(action);
-    await lscRpcProxy(supabaseClient);
+    // const action = "user_get_proxies";
+    // await clerkActions(action);
+    // await lscRpcProxy(supabaseClient);
 
-    await navStart();
-    await modalStart();
-    await iniIx2();
-    await hide_skel_dash();
+    // await navStart();
+    // await modalStart();
+    // await iniIx2();
+    // await hide_skel_dash();
     //   await pageLoader();
 }
