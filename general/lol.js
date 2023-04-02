e502cedd7b01440eda14c50633540c165f8e31a7
brkncoyot3 <52013198+brkncoyot3@users.noreply.github.com>
2023-04-02 17:08:59 +0000

    update lol.js

--- general/lol.js
+++ general/lol.js
@@ -2,8 +2,9 @@
 lol.js (c) 2023
 Desc: Demo
 Created:  2023-03-31T16:10:05.904Z
-Modified: 2023-04-02T16:57:23.608Z
+Modified: 2023-04-02T17:08:56.135Z
 */
 
 console.log('lol.js loaded!');
 console.log('Why you so funny?');
+console.log("Why making me laugh?");
