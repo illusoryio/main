2654c4641a78bd9711427c67884e622071525d4b
brkncoyot3 <52013198+brkncoyot3@users.noreply.github.com>
2023-04-02 16:17:25 +0000

    update idk.js

--- general/idk.js
+++ general/idk.js
@@ -3,10 +3,11 @@ Author: <author>
 Untitled-1 (c) 2023
 Desc: description
 Created:  2023-03-31T16:53:51.018Z
-Modified: 2023-03-31T16:54:30.097Z
+Modified: 2023-04-02T16:17:24.479Z
 Version: 1
 */
 
 console.log('Idk what to put here!');
 console.log('I guess I could put a random number generator here');
 console.log(`But I don't know how to do that yet`);
+console.log('So I guess I will just leave it like this');
