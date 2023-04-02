c12a1b45322a6598c99bc450dfb2fa6305a522b1
brkncoyot3 <52013198+brkncoyot3@users.noreply.github.com>
2023-04-02 16:57:25 +0000

    update lol.js

--- general/lol.js
+++ general/lol.js
@@ -2,36 +2,8 @@
 lol.js (c) 2023
 Desc: Demo
 Created:  2023-03-31T16:10:05.904Z
-Modified: 2023-04-02T15:50:30.079Z
+Modified: 2023-04-02T16:57:23.608Z
 */
 
-//* Load Clerk
-async function loadClerk() {
-    return new Promise((resolve) => {
-        // Get this URL from the Clerk Dashboard
-        const frontendApi = "clerk.a7i81.ec7ck.lcl.dev";
-        const version = "@latest"; // Set to appropriate version
-
-        // Creates asyncronous script
-        const script = document.createElement("script");
-        script.setAttribute("data-clerk-frontend-api", frontendApi);
-        script.async = true;
-        script.src = `https://${frontendApi}/npm/@clerk/clerk-js${version}/dist/clerk.browser.js`;
-        // Adds listener to initialize ClerkJS after it's loaded
-        script.addEventListener("load", async function () {
-            await window.Clerk.load({
-                // Set load options here...
-            });
-            console.log("Clerk initiated 🔒");
-            clerkResolved();
-        });
-        document.body.appendChild(script);
-        console.log('Clerk script loaded');
-        setTimeout(() => {
-            resolve();
-        }, 100);
-    });
-}
-
 console.log('lol.js loaded!');
-console.log('Loading Clerk...');
+console.log('Why you so funny?');
